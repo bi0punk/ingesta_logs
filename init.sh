0ae9805cdcde36b449358b9ab3e9b7f8474746b9
bi0punk <systecinfo_cl@criptext.com>
2024-12-22 18:42:27 -0300

    ffix

--- init.sh
+++ init.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
 # Crear directorios
-mkdir -p monitor/grafana/provisioning/dashboards
-mkdir -p monitor/grafana/provisioning/datasources
-mkdir -p monitor/loki
-mkdir -p monitor/prometheus
-mkdir -p monitor/promtail
-mkdir -p monitor/alertmanager
-
+mkdir -p grafana/provisioning/dashboards
+mkdir -p grafana/provisioning/datasources
+mkdir -p loki
+mkdir -p prometheus
+mkdir -p promtail
+mkdir -p alertmanager
 # Crear archivos vacíos
-touch monitor/docker-compose.yml
-
-touch monitor/grafana/provisioning/dashboards/dashboard.yml
-touch monitor/grafana/provisioning/datasources/datasource.yml
-
-touch monitor/loki/loki-config.yaml
-
-touch monitor/prometheus/prometheus.yml
-
-touch monitor/promtail/promtail-config.yaml
-
-touch monitor/alertmanager/alertmanager.yml
-
+touch docker-compose.yml
+touch grafana/provisioning/dashboards/dashboard.yml
+touch grafana/provisioning/datasources/datasource.yml
+touch loki/loki-config.yaml
+touch prometheus/prometheus.yml
+touch promtail/promtail-config.yaml
+touch alertmanager/alertmanager.yml
 echo "Estructura de directorios y archivos vacíos creada correctamente."
